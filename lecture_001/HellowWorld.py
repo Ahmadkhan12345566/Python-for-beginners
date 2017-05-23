f67cb0e4cb8eef0035ea110507d937b9fc9f4d4b
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2017-05-23 03:59:16 -0700

    HellowWorld_Intro_to_Variables

---
+++ lecture_001/HellowWorld.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 23 03:34:27 2017
+
+@author: Ahmad khan
+"""
+"""
+Python is An interpreted language
+
+there in no conncept  of terminotor(;) like in other languages 
+ 
+in python we get rid of these terminators (;) and starting ending baraks ({ })
+
+Python have very easy syntaxt 
+
+python verson  3x
+This is our 1st Progam So as try to (Disply hellwworld ) as the first 
+program of each programing language
+
+how to print(Display) hellow world in python?
+      Python use print commad to show show output
+   
+    """          
+
+
+#synatx of print ( print("somting yoy want to print")   )
+
+print("HellowWorld")
+
+print("This is our first Program")
---
+++ lecture_001/Variables.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 23 03:44:33 2017
+
+@author: Ahmad khan
+"""
+
+"""
+What is a Variable?
+A Vatiale is a memory box/cell in which we put our data 
+like 
+   num=2
+   
+  num is a variable in which we put numeric type data 
+
+
+Pythone variables do not need explicit declaration 
+
+Declaration happens auomatically when assign a value..........
+
+
+"""
+
+num=2 
+
+print(num)
+
+
+"""
+Types of variables
+
+Int 
+Float 
+String 
+Bool ....
+
+"""
+
+
+
+counter =100  #intger type 
+miles=1000.0   #float type
+name='Pakistan'#string
+desc=False;#bool (we have only two choices True or False)
+if(desc):
+    print(name)
+else:
+    print(miles)
