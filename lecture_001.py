781e72ed5467c3719692d33d49fde179f8e4e17f
Ahmadkhan12345566 <arslanahmad12345566@gmail.com>
2017-05-22 22:51:27 -0700

    data types

---
+++ lecture_001.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+"""
+Variables
+
+
+Pythone variables do not need explicit declaration 
+
+Declaration happens auomatically when assign a value..........
+
+"""
+
+counter =100  #intger type 
+miles=1000.0   #float type
+name='Pakistan'#string
+desc=False;#bool
+if(desc):#
+    print(name)
+else:
+    print(miles)
+    
+    
+    a=b=c=20 #assigning a single value to  multiple variables
+var01,var02,var03=10,3.14,'Islamabad'#assigning values to multiple variables 
+
+
+"""types of data
+        Number
+        String(there is no consept of charcter in Pythone) 
+        List
+        Tuple
+        Dictionary
+""" 
+#Number (store numeric values)
+
+var1=1
+var2=2#these are two variables of numeric type
+
+#deleting a variable
+del var01
+#deleting multipet variabkles
+del var02,var03
+"""four types of numberical types
+    int (signed integers(11))
+    long (long integers(111111111111111111111111111111111))
+    float(floating point real values(3.14))
+    complex(complex numbers(like (iota))
+"""
+
+
+#complex
+c1=complex(4,5)#assigning complex vlue to variable
+c2=complex(4,6)
+
+
+print(c1)
+#adding complex numbers
+print(c1+c2)
+#dividing complex numbers
+print(c1/c2)
